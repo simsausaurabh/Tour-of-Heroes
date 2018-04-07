3f7ae6b7b42e3201f2923540e0b681a7c3ebe2e8
simsausaurabh <saurabhsri.sri123@gmail.com>
2018-04-08 00:12:43 +0530

    Create separate hero-detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
