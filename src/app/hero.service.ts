58a3f1b69dedb73685a52291927785ccbb7c6438
simsausaurabh <saurabhsri.sri123@gmail.com>
2018-04-10 10:19:47 +0530

    Implement parameterized route

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,4 +16,10 @@ export class HeroService {
     return of(HEROES);
   }
 
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
 }
